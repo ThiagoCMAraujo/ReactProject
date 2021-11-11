f3bb10eeedfb4b6e909972ee96aa467f18198c45
tmelo387 <tmelo387@gmail.com>
2021-11-11 14:33:50 -0300

    testes com funções

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const name = 'Thiago'
+
+    function sum(a, b) {
+        return a + b
+    }
+
+    const url = 'https://via.placeholder.com/150'
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                
+                <p>Soma: {sum(1, 3)}</p>
+                <img src={url} alt = ""/>    
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Learn React with {name}
+
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
