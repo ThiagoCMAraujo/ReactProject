96752156827a85ced1fd03659a0eceaa1c5bf1a1
tmelo387 <tmelo387@gmail.com>
2021-11-13 11:52:58 -0300

    testes com onChange

--- react-project/src/components/Forms.js
+++ react-project/src/components/Forms.js
@@ -1,22 +1,32 @@
-function Form({}) {
-
-    function cadastrarUsuario(e){
+import { useState } from 'react'
+function Form() {
+    function cadastrarUsuario(e) {
         e.preventDefault()
-        console.log("Cadastrou o usuário")
+        console.log(`Nome: ${name}`)
+        console.log(`Password: ${password}`)
+        console.log(name)
     }
-    return(
-<div>
-    <h1>Cadastro: </h1>
-        <form onSubmit={cadastrarUsuario}>
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
+    return (
         <div>
-            <input type="text" placeholder="Digite seu nome"></input>
+            <h1>Cadastro: </h1>
+            <form onSubmit={cadastrarUsuario}>
+                <div>
+                    <label htmlFor="name">Nome:</label>
+                    {/* value={name} para edits*/}
+                    <input type="text" id="name" name="name" placeholder="Digite seu nome"  onChange={(e) => setName(e.target.value)}></input>
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input type="password" id="password" name="password" placeholder="Senha" onChange={(e)=> setPassword(e.target.value)} /></div>
+                <div>
+                    <input type="submit" value="Cadastrar" />
+                </div>
+            </form>
+
         </div>
-        <div>
-            <input type="submit" value="Cadastrar" />
-        </div>
-        </form>
-    
-</div>
     )
 }
 export default Form;
